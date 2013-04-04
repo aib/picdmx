9b8e3743d68c674375639b1cf080d338652cce0c
aib <aibok42@gmail.com>
2013-04-04 14:47:22 +0300

    Make a 6-channel PWM example

--- dmxr.c
+++ dmxr.c
@@ -17,12 +17,48 @@
 
 void main(void)
 {
-	dmxAddr = 3;
+	TRISD = 0;
+	PORTD = 0;
+	dmxAddrBase = 3;
 	dmx_setup();
+
+	OPTION_REGbits.T0CS = 0;
+	OPTION_REGbits.PSA = 0;
+	OPTION_REGbits.PS = 2;
+
 	INTCONbits.GIE = 1;
 
 	while (1) {
-		PORTDbits.RD5 = dmxVal;
+		if (TMR0 < dmxVal[0]) {
+			PORTDbits.RD0 = 1;
+		} else {
+			PORTDbits.RD0 = 0;
+		}
+		if (TMR0 < dmxVal[1]) {
+			PORTDbits.RD1 = 1;
+		} else {
+			PORTDbits.RD1 = 0;
+		}
+		if (TMR0 < dmxVal[2]) {
+			PORTDbits.RD2 = 1;
+		} else {
+			PORTDbits.RD2 = 0;
+		}
+		if (TMR0 < dmxVal[3]) {
+			PORTDbits.RD3 = 1;
+		} else {
+			PORTDbits.RD3 = 0;
+		}
+		if (TMR0 < dmxVal[4]) {
+			PORTDbits.RD4 = 1;
+		} else {
+			PORTDbits.RD4 = 0;
+		}
+		if (TMR0 < dmxVal[5]) {
+			PORTDbits.RD5 = 1;
+		} else {
+			PORTDbits.RD5 = 0;
+		}
 	}
 }
 
